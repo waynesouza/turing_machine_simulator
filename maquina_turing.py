df9155b00c8f930fe69256d34f7b85b6cff4275c
waynesouza <waynesouza07@gmail.com>
2023-06-09 14:34:30 -0300

    feat: added file to check args, fixed function to open terminal and some fixes

--- maquina_turing.py
+++ maquina_turing.py
@@ -1,5 +1,6 @@
 import util.constante as c
 import util.parametro as p
+import handle.analisador as a
 
 
 class MaquinaTuring:
@@ -21,20 +22,22 @@ class MaquinaTuring:
 
     def verificar_aceitacao(self, fita, bloco_atual):
         estado_atual = bloco_atual.estado_inicial
+
         while estado_atual != c.PARE and estado_atual != c.RETORNE:
             self.computacoes -= 1
+
             if self.computacoes < 0:
-                # TODO funcao para abrir terminal (dentro do if)
-                return
+                self.abrir_terminal()
+
             self.verificar_verbose(bloco_atual, estado_atual, fita)
             simbolo_lido = fita[self.indice]
             comandos_do_estado = list(filter(lambda comando_analisado: comando_analisado.estado_atual == estado_atual,
                                              bloco_atual.comandos))
             comando_atual = comandos_do_estado[0]
+
             if len(comandos_do_estado) == 1 and comando_atual.chamada_outro_bloco:
                 if comando_atual.breakpoint:
-                    # TODO funcao para abrir terminal (dentro do if)
-                    return
+                    self.abrir_terminal()
                 self.verificar_aceitacao(fita, list(filter(lambda bloco: bloco.id == comando_atual.id_bloco,
                                                            self.blocos))[0])
                 estado_atual = comando_atual.estado_retorno
@@ -44,17 +47,20 @@ class MaquinaTuring:
                 comandos_sem_coringa = list(filter(lambda comando_analisado:
                                                    comando_analisado.simbolo_atual == simbolo_lido, comandos_do_estado))
                 comando = None
+
                 try:
                     comando = comandos_coringa[0] if len(comandos_sem_coringa) == 0 else comandos_sem_coringa[0]
                 except Exception as e:
                     # FIXME criar arquivo de erros
                     print(f'ERROR -- símbolo: {simbolo_lido}, estado: {estado_atual} e bloco: {bloco_atual.id}')
                     print(f'Detalhes: {e}')
+
                 if comando.breakpoint:
-                    # TODO funcao para abrir terminal (dentro do if)
-                    return
+                    self.abrir_terminal()
+
                 fita[self.indice] = comando.novo_simbolo if comando.novo_simbolo != c.CORINGA else fita[self.indice]
                 estado_atual = comando.novo_estado if comando.novo_estado != c.CORINGA else estado_atual
+
                 if comando.movimento != c.IMOVEL:
                     if self.indice == 0 and comando.movimento == c.ESQUERDA:
                         fita.insert(0, c.BRANCO)
@@ -62,25 +68,34 @@ class MaquinaTuring:
                     if self.indice == (len(fita) - 1) and comando.movimento == c.DIREITA:
                         fita.append(c.BRANCO)
                     self.indice += 1 if comando.movimento == c.DIREITA else -1
+
         self.verificar_verbose(bloco_atual, estado_atual, fita)
 
     def abrir_terminal(self):
         novas_instrucoes = input('Informe as novas instruções: ').split(' ')  # FIXME adicionar um arquivo de mensagens
+
         if novas_instrucoes[0] == c.VAZIO:
-            novas_instrucoes = ''  # TODO usar as ultimas instruções
-        # TODO adicionar else e o restante da lógica (argumentos)
+            novas_instrucoes = p.ultimas_instrucoes
+        else:
+            p.ultimas_instrucoes = novas_instrucoes
+
+        a.verificar_argumentos(novas_instrucoes)
         self.computacoes = p.computacoes
 
     def imprimir(self, bloco_atual, estado_atual, fita_original):
         bloco = c.PONTO * (16 - len(bloco_atual.id)) + bloco_atual.id
         estado = c.ZERO * (4 - len(estado_atual)) + estado_atual
+
         fita_com_maximo_brancos = f'{c.BRANCO + (c.MAXIMO_BRANCO_FITA - 1)}' + ''.join(fita_original) + \
                                   f'{c.BRANCO + (c.MAXIMO_BRANCO_FITA - 1)}'
+
         indice_esquerda = self.indice
         indice_centro = self.indice + c.MAXIMO_BRANCO_FITA
         indice_direita = self.indice + ((2 * c.MAXIMO_BRANCO_FITA) - 1)
+
         resultado_fita = c.VAZIO
         indice_atual = indice_esquerda
+
         while indice_atual < indice_direita:
             if (indice_atual + 1) == indice_centro:
                 resultado_fita = f'{resultado_fita}{p.delimitador_inicial}' \
@@ -88,6 +103,7 @@ class MaquinaTuring:
             else:
                 resultado_fita = f'{resultado_fita}{fita_com_maximo_brancos[indice_atual]}'
             indice_atual += 1
+
         print(f'{bloco}.{estado}: {resultado_fita}')
 
     def verificar_verbose(self, bloco_atual, estado_atual, fita_original):
